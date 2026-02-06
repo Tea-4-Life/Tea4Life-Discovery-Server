fbbdc3603db94734c4988b75e5310dcbca1976d6
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-02-06 13:55:34 +0700

    first init

---
+++ src/test/java/tea4life/discovery_server/BaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tea4life.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
